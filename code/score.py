8bb54477fffa15cf9fcb112b73a5abbbf5d724e1
ychen976 <72422735+ychen976@users.noreply.github.com>
2020-11-30 04:01:03 -0600

    Update score.py
    added a output function.

--- code/score.py
+++ code/score.py
@@ -7,7 +7,7 @@ Created on Sat Nov 28 13:38:53 2020
 """
 
 import pandas as pd
-df=pd.read_csv("../Data/dessert_clean.csv")
+df=pd.read_csv("dessert_clean.csv")
 print(df.head(5))
 names=df['business_id'].drop_duplicates().values.tolist()
 stars=df['stars_x'].tolist()
@@ -46,22 +46,58 @@ new_document=DataFrame(cc)
 def features(words):
     return dict([word,True] for word in words)
 
+from pandas.core.frame import DataFrame
+def show_words(classifier,outcome,current_id):
+        # Determine the most relevant features, and display them.
+    cpdist = classifier._feature_probdist
+    a=[]
+    b=[]
+    c=[]
+    d=[]
+    for (fname, fval) in outcome:
+
+        def labelprob(l):
+            return cpdist[l, fname].prob(fval)
+
+        labels = sorted(
+            [l for l in classifier._labels if fval in cpdist[l, fname].samples()],
+            key=lambda element: (-labelprob(element), element),
+            reverse=True
+        )
+        if len(labels) == 1:
+            continue
+        l0 = labels[0]
+        l1 = labels[-1]
+        if cpdist[l0, fname].prob(fval) == 0:
+            ratio = "INF"
+        else:
+            ratio = "%8.1f" % (
+                cpdist[l1, fname].prob(fval) / cpdist[l0, fname].prob(fval)
+            )
+        a.append(current_id)
+        b.append(fname)
+        c.append([l1,l0])
+        d.append(ratio)
+    e={"id": a,
+       "word": b,
+       "pos/neg": c,
+       "ratio": d}
+    data=DataFrame(e)
+    return(data)
+
 def words_score(current_doc,current_id):
     pos_t=current_doc.text[current_doc['p_n']=="positive"]
     neg_t=current_doc.text[current_doc['p_n']=="negative"]
     labeled=([(features(text),'pos') for text in pos_t]+[(features(text),'neg') for text in neg_t])
     classifier = nltk.NaiveBayesClassifier.train(labeled)
-    classifier.show_most_informative_features(500)
     outcome=classifier.most_informative_features(500)
-    type(outcome)
-    i_d=current_id
-    f=open(i_d+'.txt','w+')
-    f.write(" ".join('%s'%id for id in outcome))
-    f.close
+    id_words=show_words(classifier,outcome,current_id)
+    return(id_words)
 
+words = pd.DataFrame()
 for name in names:
     current_id=name
     current_doc=new_document[new_document['business_id']==current_id]
-    words_score(current_doc,current_id)
-    
-# Train a Naive Bayes model and show the most informative features 
+    words=words.append(words_score(current_doc,current_id))
+words.to_csv("words_score.csv",index=False,header=True)   
+ 
